f2b7c51b445a87184344832a3e0df63ef63a435e
cuipf0823 <cuipf0823@gmail.com>
2017-06-16 18:20:54 +0800

    modify config files

--- auto_deploy.sh
+++ auto_deploy.sh
@@ -1,11 +1,49 @@
 #!/bin/bash
-#1. support Debian and ubuntu platforms
+#1. support Debian or ubuntu platforms
 #2. using root execute shell
 #3. install mysql version 5.6
 #4. Using the MySQL APT Repository
 
-#config
+# config
+# MySQL DownLoad URL
 MYSQL_DOWNLOAD_URL=https://repo.mysql.com//mysql-apt-config_0.8.6-1_all.deb
+# MySQL IP address
+MYSQL_IP=''
+# Usually will not change
+MYSQL_PORT='3306'
+MYSQL_USER='root'
+MYSQL_PWD='123456'
+PHPMYADMIN_URL='http://10.1.1.1/phpmyadmin/'
+
+
+os_str=$(lsb_release -i | awk  '{print $3}')
+if [[ x"${os_str:0:6}" = x"Ubuntu" || x"${os_str:0:6}" = x"Debian" ]]; then
+  echo "Current OS: $os_str start deploy..."
+else
+  echo "Current OS: $os_str not supported exit "
+  exit 0
+fi
+
+set_value()
+{
+	local file=$1
+	local key=$2
+	local value=$3
+	if [ -z $file -o -z $key -o -z $value ];then
+		return 1
+	fi
+
+	if [ -f $file ] && cat $file | grep "^$key" > /dev/null; then
+		sed -i "/^$key/d" $file
+	fi
+
+	echo "${key} = ${value}" >> $file
+	return 0
+}
+
+set_value ./config.inc.php "\$cfg['PmaAbsoluteUri']" $PHPMYADMIN_URL
+
+exit 0
 
 # down mysql apt Repository
 pkg_name=$(echo $MYSQL_DOWNLOAD_URL | awk -F '//' '{print $3}')
@@ -13,7 +51,7 @@ if [[ ! -e $pkg_name ]]; then
   wget $MYSQL_DOWNLOAD_URL
 fi
 
-if [[ x$(whereis mysql) = "xmysql:" ]]; then
+if [[ x$(whereis mysql) = x"mysql:" ]]; then
   echo "current system not exit mysql. install mysql ..."
   # During the installation of the package, you will be asked to choose the versions of the MySQL server and other components
   dpkg -i $pkg_name
@@ -34,6 +72,47 @@ else
   fi
 fi
 
+# mysql config
+\cp ./my.cnf /etc/my.cnf
+
 # install apache2
+if [[ x$(whereis apache2) = x"apache2:" ]]; then
+  apt-get install -y apache2
+fi
+
+# install php
+apt-get install -y php5
+
+# install other components
+apt-get install -y libapache2-mod-php5 php5-gd php5-mysql
+
+# install phpmyadmin
+apt-get install -y phpmyadmin
+
+# soft link
+ln -s /usr/share/phpmyadmin /var/www/html
+echo 'Include /etc/phpmyadmin/apache.conf' >> /etc/apache2/apache2.conf
+service apache2 restart
+
+# Modify phpmyadmin config
+# first backup
+\cp /etc/phpmyadmin/config.inc.php /etc/phpmyadmin/config.inc.php.bk
+
+set_value()
+{
+	local file=$1
+	local key=$2
+	local value=$3
+	if [ -z $file -o -z $key -o -z $value ];then
+		return 1
+	fi
+
+	if [ -f $file ] && cat $file | grep "^$key" > /dev/null; then
+		sed -i "/^$key/d" $file
+	fi
+
+	echo "${key} = ${value}" >> $file
+	return 0
+}
 
 exit 0
--- config.inc.php
+++ config.inc.php
@@ -10,11 +10,12 @@
  * @package PhpMyAdmin
  */
 
+$cfg['PmaAbsoluteUri'] = 'http://10.0.1.201:8385/phpmyadmin/';
 /*
  * This is needed for cookie based authentication to encrypt password in
  * cookie
  */
-$cfg['blowfish_secret'] = 'a8b7c6d'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+$cfg['blowfish_secret'] = 'ad3b2c3'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
 
 /*
  * Servers configuration
@@ -28,20 +29,34 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = '10.10.10.103';
+$cfg['Servers'][$i]['host'] = '';
 $cfg['Servers'][$i]['connect_type'] = 'tcp';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
+
+/**
+ * MySQL user
+ *
+ * @global string $cfg['Servers'][$i]['user']
+ */
+$cfg['Servers'][$i]['user'] = 'root';
+
+/**
+ * MySQL password (only needed with 'config' auth_type)
+ *
+ * @global string $cfg['Servers'][$i]['password']
+ */
+$cfg['Servers'][$i]['password'] = '123456';
 
 /*
  * phpMyAdmin configuration storage settings.
  */
 
 /* User used to manipulate with storage */
-// $cfg['Servers'][$i]['controlhost'] = '';
-// $cfg['Servers'][$i]['controlport'] = '';
-// $cfg['Servers'][$i]['controluser'] = 'pma';
-// $cfg['Servers'][$i]['controlpass'] = 'pmapass';
+$cfg['Servers'][$i]['controlhost'] = '';
+$cfg['Servers'][$i]['controlport'] = '';
+$cfg['Servers'][$i]['controluser'] = 'root';
+$cfg['Servers'][$i]['controlpass'] = '123456';
 
 /* Storage database and tables */
 // $cfg['Servers'][$i]['pmadb'] = 'phpmyadmin';
@@ -64,95 +79,8 @@ $cfg['Servers'][$i]['AllowNoPassword'] = false;
 // $cfg['Servers'][$i]['savedsearches'] = 'pma__savedsearches';
 /* Contrib / Swekey authentication */
 // $cfg['Servers'][$i]['auth_swekey_config'] = '/etc/swekey-pma.conf';
-
-/*
- * End of servers configuration
- */
-
-/*
- * Directories for saving/loading files from server
- */
 $cfg['UploadDir'] = '';
 $cfg['SaveDir'] = '';
+$cfg['DefaultLang'] = 'zh';
 
-/**
- * Whether to display icons or text or both icons and text in table row
- * action segment. Value can be either of 'icons', 'text' or 'both'.
- */
-//$cfg['RowActionType'] = 'both';
-
-/**
- * Defines whether a user should be displayed a "show all (records)"
- * button in browse mode or not.
- * default = false
- */
-//$cfg['ShowAll'] = true;
-
-/**
- * Number of rows displayed when browsing a result set. If the result
- * set contains more rows, "Previous" and "Next".
- * default = 30
- */
-//$cfg['MaxRows'] = 50;
-
-/**
- * disallow editing of binary fields
- * valid values are:
- *   false    allow editing
- *   'blob'   allow editing except for BLOB fields
- *   'noblob' disallow editing except for BLOB fields
- *   'all'    disallow editing
- * default = blob
- */
-//$cfg['ProtectBinary'] = 'false';
-
-/**
- * Default language to use, if not browser-defined or user-defined
- * (you find all languages in the locale folder)
- * uncomment the desired line:
- * default = 'en'
- */
-//$cfg['DefaultLang'] = 'en';
-//$cfg['DefaultLang'] = 'de';
-
-/**
- * default display direction (horizontal|vertical|horizontalflipped)
- */
-//$cfg['DefaultDisplay'] = 'vertical';
-
-
-/**
- * How many columns should be used for table display of a database?
- * (a value larger than 1 results in some information being hidden)
- * default = 1
- */
-//$cfg['PropertiesNumColumns'] = 2;
-
-/**
- * Set to true if you want DB-based query history.If false, this utilizes
- * JS-routines to display query history (lost by window close)
- *
- * This requires configuration storage enabled, see above.
- * default = false
- */
-//$cfg['QueryHistoryDB'] = true;
-
-/**
- * When using DB-based query history, how many entries should be kept?
- *
- * default = 25
- */
-//$cfg['QueryHistoryMax'] = 100;
-
-/**
- * Should error reporting be enabled for JavaScript errors
- *
- * default = 'ask'
- */
-//$cfg['SendErrorReports'] = 'ask';
-
-/*
- * You can find more configuration options in the documentation
- * in the doc/ folder or at <http://docs.phpmyadmin.net/>.
- */
 ?>
